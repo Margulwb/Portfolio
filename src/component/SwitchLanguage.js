dae00d23e8f81e86909bb38dd54f1827da844f64
Margul <margulwb@gmail.com>
2025-04-28 13:24:37 +0200

    change description

--- src/component/SwitchLanguage.js
+++ src/component/SwitchLanguage.js
@@ -5,10 +5,14 @@ export function SwitchLanguage(language) {
                 heading:"Hi, I'm Maciek",
                 aFewWordAboutMe:
                 <>
-                    <p>I am an ambitious 20 years old who wants to become the best Front-End developer.</p>
-                    <p>I've always been curious about how games and apps work with a main interest on how the development process looks like.</p>
-                    <span>Creating pages from a visual point of view is not my strong point,
-                        because my favorite part of creating a page is logic in JS.</span>
+                    DevOps Engineer with over 2.5 years of experience in designing and maintaining CI/CD processes, managing infrastructure using IaC
+                    tools (Terraform, Ansible), and containerizing applications with Docker. I optimized CI processes in monorepositories, allowing for more
+                    efficient management and deployment of applications across multiple projects. I migrated CI/CD processes from Jenkins to Azure DevOps,
+                    simplifying pipeline management and increasing the efficiency of development teams. Additionally, I created standards for IT projects in
+                    Azure DevOps, which enabled process optimization and accelerated the introduction of new functionalities into the system. The entire
+                    infrastructure and CI/CD configuration was written in Terraform, ensuring full automation and easier environment management. Currently, I
+                    am expanding my skills in Kubernetes and GitOps. I focus on automation, security, and effective collaboration with development teams to
+                    ensure stable and efficient environments.
                 </>,
             },
             techStack:{
@@ -26,11 +30,15 @@ export function SwitchLanguage(language) {
                 heading:"Cześć, tutaj Maciek",
                 aFewWordAboutMe:
                 <>
-                    <p>Jestem ambitnym 20 latkiem, który chce zostać najlepszym Front-End developerem.</p>
-                    <p>Zawsze byłem ciekaw jak działają gry i aplikacje, a najbardziej
-                        interesowało mnie jak wygląda proces ich tworzenia.</p>
-                    <span>Tworzenie stron z wizualnego punktu widzenia nie jest moją
-                        mocną stroną, ponieważ moją ulubioną częścią tworzenia stron jest logika w JS.</span>
+                    DevOps Engineer z ponad 2,5-letnim doświadczeniem w projektowaniu i utrzymywaniu procesów CI/CD, zarządzaniu infrastrukturą za
+                    pomocą narzędzi IaC (Terraform, Ansible) oraz konteneryzacją aplikacji w Docker. Zoptymalizowałem procesy CI w monorepozytoriach, co
+                    pozwoliło na bardziej efektywne zarządzanie i wdrażanie aplikacji w wielu projektach. Dokonałem migracji procesów CI/CD z Jenkins na
+                    Azure DevOps, co pozwoliło na uproszczenie zarządzania pipeline'ami oraz zwiększenie wydajności zespołów deweloperskich.
+                    Dodatkowo stworzyłem standardy projektów informatycznych w Azure DevOps, co umożliwiło optymalizację procesów oraz przyspieszenie
+                    wprowadzania nowych funkcjonalności do systemu. Cała konfiguracja infrastruktury oraz procesów CI/CD została napisana w Terraformie,
+                    co zapewniło pełną automatyzację i łatwiejsze zarządzanie środowiskami. Obecnie rozwijam kompetencje w zakresie Kubernetes oraz
+                    GitOps. Skupiam się na automatyzacji, bezpieczeństwie oraz efektywnej współpracy z zespołami deweloperskimi w celu zapewnienia
+                    stabilnych i wydajnych środowisk
                 </>,
             },
             techStack:{
